8e1189780faabb3134061597db01a4f23fc1b035
james <ivanlavan773@gmail.com>
2025-07-10 12:41:13 +0300

    Updated code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-function NavBar() {
+function NavBar(user) {
   return (
     <nav>
+      <span>Welcome, {user.name}!</span>
       <a href="#home">Home</a>
       <a href="#about">About</a>
     </nav>
